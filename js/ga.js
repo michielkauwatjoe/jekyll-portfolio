7eee3e1ed2a9f8982735a6f4818845317d813820
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2016-09-18 15:51:49 +0200

    GA tracking code.

--- js/ga.js
+++ js/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-39485584-1', 'auto');
+ga('create', 'UA-18382769-1', 'auto');
 ga('send', 'pageview');
