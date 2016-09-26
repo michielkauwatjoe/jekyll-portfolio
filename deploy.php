9eda8651c051887caee87f9b4c84284b72303ea8
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2016-09-26 14:02:42 +0200

    Trying python.

--- deploy.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/michielkauwatjoe/ && git pull && bundle exec jekyll build' );
-}
-?>
